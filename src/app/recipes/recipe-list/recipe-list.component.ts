1a33892584204eae91ef2a3c6460617c2e1e8652
missvenus <missvenus.jain@gmail.com>
2017-06-10 08:56:49 +0100

    recipes components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','this is simply a test recipe','https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Recipe-575434.svg/2000px-Recipe-575434.svg.png'),
+    new Recipe('A Test Recipe','this is simply a test recipe','https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Recipe-575434.svg/2000px-Recipe-575434.svg.png')
+
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
